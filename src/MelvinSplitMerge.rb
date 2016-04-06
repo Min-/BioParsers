3bd49892fed8491d625f21cba3f9c34d1ad5e231
Min- <min_z@outlook.com>
2016-04-06 15:22:26 -0500

    fix ruby

--- src/MelvinSplitMerge.rb
+++ src/MelvinSplitMerge.rb
@@ -16,15 +16,15 @@ def splitAndMerge(nl, dir, file1, file2, output)
   %x{cat #{dir + "/tmp3/xa*"} > #{output}}
 end
 
-nl = ARGV[0]
-dir = ARGV[1]
-file1 = ARGV[2]
-file2 = ARGV[3]
-output = ARGV[4]
+@nl = ARGV[0]
+@dir = ARGV[1]
+@file1 = ARGV[2]
+@file2 = ARGV[3]
+@output = ARGV[4]
 
 def main
   puts "MelvinSplitMerge 40000000 /media/martinlab/analysis/mz_P12_heart_dropseq/raw mh1.s1.l2.r1 mh1.s1.l2.r2 mh1.s1.l2" 
-  splitAndMerge(nl, dir, file1, file2, output)
+  splitAndMerge(@nl, @dir, @file1, @file2, @output)
 end
 
 main
